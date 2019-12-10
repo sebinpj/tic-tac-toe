df359d547e6dd1a54fce50197cff072a81a99204
Sebin P Johnson <1468974-sebinpj@users.noreply.gitlab.com>
2019-12-10 16:54:53 +0530

    SEBIN | Github Pages finishing touches for functionality

---
+++ docs/precache-manifest.60010c635ae18eecd0ae4b57538919e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1271d199bd63d97b7408d9567ca94b0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e66c74ae10ad5606ae46",
+    "url": "/static/css/main.30052815.chunk.css"
+  },
+  {
+    "revision": "c8a761505384f81d6378",
+    "url": "/static/js/2.e377739a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e377739a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e66c74ae10ad5606ae46",
+    "url": "/static/js/main.e85a4a92.chunk.js"
+  },
+  {
+    "revision": "057779023b6d62fbc37c",
+    "url": "/static/js/runtime-main.36bb92ed.js"
+  }
+]);
