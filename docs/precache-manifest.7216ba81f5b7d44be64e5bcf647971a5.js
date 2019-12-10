9e0a6bf1f6370d798c527bf09bc3bf74664458df
Sebin P Johnson <1468974-sebinpj@users.noreply.gitlab.com>
2019-12-10 12:58:21 +0530

    SEBIN | Adding docs

---
+++ docs/precache-manifest.7216ba81f5b7d44be64e5bcf647971a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b2d4ecd6727673d52fdfd024173e59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83cfe158b5edbd6ae834",
+    "url": "/static/css/main.30052815.chunk.css"
+  },
+  {
+    "revision": "c8a761505384f81d6378",
+    "url": "/static/js/2.e377739a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e377739a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83cfe158b5edbd6ae834",
+    "url": "/static/js/main.0aa48ce7.chunk.js"
+  },
+  {
+    "revision": "057779023b6d62fbc37c",
+    "url": "/static/js/runtime-main.36bb92ed.js"
+  }
+]);
